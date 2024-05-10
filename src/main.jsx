11e3d38afe453066bdb2205b6f7d10d8ca2f2f26
Ramin Karimpor <ramin.kp81@gmail.com>
2024-05-10 16:25:11 +0330

    fixed

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,9 @@ import { BrowserRouter } from "react-router-dom";
 import { StyledEngineProvider } from "@mui/material";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </StyledEngineProvider>
-  // {/* </React.StrictMode> */}
+  <StyledEngineProvider injectFirst>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StyledEngineProvider>
 );
